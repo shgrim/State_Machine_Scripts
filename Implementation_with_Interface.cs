33a1a87d4d47c125019783acb2ce2f186b0e18be
Raghib Khan <khan03raghib@gmail.com>
2023-12-17 03:59:13 +0530

    Using Enum, Interface

---
+++ Implementation_with_Interface.cs
@@ -0,0 +1,62 @@
+﻿public interface IState
+{
+    public void Enter();
+    public void Execute();
+    public void Exit();
+}
+
+public class StateMachine
+{
+    IState currentState;
+
+    public void ChangeState(IState newState)
+    {
+        if (currentState != null)
+            currentState.Exit();
+
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    public void Update()
+    {
+        if (currentState != null) currentState.Execute();
+    }
+}
+
+public void TestState : IState
+{
+    Unit owner;
+
+public TestState(Unit owner) { this.owner = owner; }
+
+public void Enter()
+{
+    Debug.Log("entering test state");
+}
+
+public void Execute()
+{
+    Debug.Log("updating test state");
+}
+
+public void Exit()
+{
+    Debug.Log("exiting test state");
+}
+}
+ 
+public class Unit : MonoBehaviour
+{
+    StateMachine stateMachine = new StateMachine();
+
+    void Start()
+    {
+        stateMachine.ChangeState(new TestState(this));
+    }
+
+    void Update()
+    {
+        stateMachine.Update();
+    }
+}
---
+++ Implementation_with_enum.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Statemachine_FSM : MonoBehaviour
+{
+
+    public enum State
+    {
+        Idle,
+        Initialize,
+        Setup,
+        Combat,
+        Looting
+    }
+
+    public enum Combat
+    {
+        FindEnemy,
+        AttackEnemy,
+        EnemyDead
+    }
+
+    private State _state; //Local variable that represents our state
+    public Combat combat;
+
+    IEnumerator Start()
+    {
+        _state = State.Initialize;
+        combat = Combat.FindEnemy;
+
+        while (true)
+        {
+            switch (_state)
+            {
+                case State.Initialize:
+                    InitMe();
+                    break;
+                case State.Setup:
+                    SetMeUp();
+                    break;
+                case State.Combat:
+                    InCombat();
+                    break;
+                case State.Looting:
+                    Looting();
+                    break;
+            }
+            yield return 0;
+        }
+    }
+
+    private void InitMe()
+    {
+        Debug.Log("This is the InitMe function");
+        _state = State.Setup;
+    }
+    private void SetMeUp()
+    {
+        Debug.Log("This is the SetMeUp function");
+        _state = State.Combat;
+    }
+    private void InCombat()
+    {
+        Debug.Log("In Combat");
+
+        switch (combat)
+        {
+            case Combat.FindEnemy:
+                FindEnemy();
+                break;
+            case Combat.AttackEnemy:
+                AttackEnemy();
+                break;
+            case Combat.EnemyDead:
+                EnemyDead();
+                break;
+        }
+    }
+    private void Looting()
+    {
+        Debug.Log("Looting");
+        _state = State.Idle;
+    }
+
+    private void FindEnemy()
+    {
+        Debug.Log("Finding Enemy");
+        combat = Combat.AttackEnemy;
+    }
+    private void AttackEnemy()
+    {
+        Debug.Log("Attacking Enemy");
+        combat = Combat.EnemyDead;
+    }
+    private void EnemyDead()
+    {
+        Debug.Log("Enemy Dead");
+        combat = Combat.FindEnemy;
+        _state = State.Looting;
+    }
+}
